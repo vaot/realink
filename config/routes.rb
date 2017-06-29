318d5c6cb088a4a19fd6052548bf40e578078ef6
Steven Yan <stevenyan2015@gmail.com>
2017-06-28 18:25:28 -0700

    add notifications

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
+  get '/notifications', to: 'notifications#index'
+  get '/notifications/:id', to: 'notifications#show'
 end
