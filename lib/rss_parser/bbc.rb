260352af8a2144ec3cda9c9d9c4211accd9966e6
jwc1 <joon0406@hotmail.com>
2017-07-12 20:00:57 -0700

    updating bbc

--- lib/rss_parser/bbc.rb
+++ lib/rss_parser/bbc.rb
@@ -4,7 +4,9 @@ module RssParser
     URLS = {
       world: "http://feeds.bbci.co.uk/news/world/rss.xml",
       technology: "http://feeds.bbci.co.uk/news/technology/rss.xml",
-      business: "http://feeds.bbci.co.uk/news/business/rss.xml"
+      business: "http://feeds.bbci.co.uk/news/business/rss.xml",
+      uk: "http://feeds.bbci.co.uk/news/uk/rss.xml"
+
     }
 
     def self.name
@@ -12,7 +14,9 @@ module RssParser
     end
 
     def self.availables
-      %i(world technology business)
+      %i(world technology business uk)
     end
   end
 end
+
+# RssParser::Bbc.new(type: :uk).parse
