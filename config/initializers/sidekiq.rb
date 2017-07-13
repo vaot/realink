90779c30ff7e7b5e25ab92aa7e6570cbd8488024
vaot <victorandrey_123@hotmail.com>
2017-07-12 19:04:41 -0700

    Should be sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,13 @@
 def redis_config
   @redis_config ||= YAML.load(
     ERB.new(File.read("#{Rails.root}/config/redis.yml")).result
-  )[Rails.env]
+  )[Rails.env].with_indifferent_access
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = redis_config
+  config.redis = redis_config[:sidekiq]
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = redis_config
+  config.redis = redis_config[:sidekiq]
 end
