e13df9e9550db58e4d3f58fd87a8231585cd911a
jwc1 <joon0406@hotmail.com>
2017-07-18 21:50:30 -0700

    more stuff for bbc

--- lib/rss_parser/bbc.rb
+++ lib/rss_parser/bbc.rb
@@ -5,8 +5,13 @@ module RssParser
       world: "http://feeds.bbci.co.uk/news/world/rss.xml",
       technology: "http://feeds.bbci.co.uk/news/technology/rss.xml",
       business: "http://feeds.bbci.co.uk/news/business/rss.xml",
-      uk: "http://feeds.bbci.co.uk/news/uk/rss.xml"
-
+      uk: "http://feeds.bbci.co.uk/news/uk/rss.xml",
+      politics: "http://feeds.bbci.co.uk/news/politics/rss.xml",
+      health: "http://feeds.bbci.co.uk/news/health/rss.xml",
+      education: "http://feeds.bbci.co.uk/news/education/rss.xml",
+      science: "http://feeds.bbci.co.uk/news/science_and_environment/rss.xml",
+      technology: "http://feeds.bbci.co.uk/news/technology/rss.xml",
+      entertainment: "http://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml"
     }
 
     def self.name
@@ -14,7 +19,7 @@ module RssParser
     end
 
     def self.availables
-      %i(world technology business uk)
+      %i(world technology business uk politics health education science technology entertainment)
     end
   end
 end
